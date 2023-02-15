99fd6973868ed733608a68e6fbc5086eb6f209b5
unknown <edisonalexanderarias@gmail.com>
2023-02-14 23:00:17 -0800

    added two intergers together

--- bool.cpp
+++ bool.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
 int main() {
-  bool isCodingFun = true;
-  bool isFishTasty = false;
-  std::cout << isCodingFun << "\n";
-  std::cout << isFishTasty;
+  int myNum = 15, Num = 13;
+  std::cout << myNum + Num;
   return 0;
 }
+
+// #include <iostream>
+
+// int main() {
+//   bool isCodingFun = true;
+//   bool isFishTasty = false;
+//   std::cout << isCodingFun << "\n";
+//   std::cout << isFishTasty;
+//   return 0;
+// }
