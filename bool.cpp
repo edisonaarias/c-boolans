e8c057fa488d1575e93a31e8602a53c672894d7f
unknown <edisonalexanderarias@gmail.com>
2023-02-15 21:05:54 -0800

    Added a while loop

--- bool.cpp
+++ bool.cpp
@@ -37,12 +37,24 @@
 //   return 0;
 // }
 
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   int x = 10;
+//   x += 5;
+//   cout << x;
+//   return 0;
+// }
+
 #include <iostream>
 using namespace std;
 
 int main() {
-  int x = 10;
-  x += 5;
-  cout << x;
+  int i = 0;
+  while (i < 5) {
+    cout << i << "\n";
+    i++;
+  }
   return 0;
 }
