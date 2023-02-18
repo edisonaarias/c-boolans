af5e5887d37fccf530fc1913cb062bc427c8e26d
unknown <edisonalexanderarias@gmail.com>
2023-02-17 23:34:06 -0800

    Added a continue statement

--- bool.cpp
+++ bool.cpp
@@ -69,15 +69,28 @@
 //   return 0;
 // }
 
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   for (int i = 0; i < 10; i++) {
+//     if (i == 4) {
+//       break;
+//     }
+//     cout << i << "\n";
+//   } 
+//   return 0;
+// }
+
 #include <iostream>
 using namespace std;
 
 int main() {
   for (int i = 0; i < 10; i++) {
     if (i == 4) {
-      break;
+      continue;
     }
     cout << i << "\n";
-  } 
+  }   
   return 0;
 }
