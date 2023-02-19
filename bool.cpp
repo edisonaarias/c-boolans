eda786b4025390d976d0c721746d78bf0f1167ff
unknown <edisonalexanderarias@gmail.com>
2023-02-18 23:26:59 -0800

    added an array

--- bool.cpp
+++ bool.cpp
@@ -94,3 +94,13 @@ int main() {
   }   
   return 0;
 }
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
+  cout << cars[0];
+  return 0;
+}
