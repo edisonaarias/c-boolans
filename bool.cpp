4150966d6ca0f269152053fcea40938c54067000
unknown <edisonalexanderarias@gmail.com>
2023-02-20 23:41:07 -0800

    added a reference

--- bool.cpp
+++ bool.cpp
@@ -121,3 +121,16 @@ int main() {
   cout << myStructure.myString << "\n";
   return 0;
 }
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string food = "Pizza";
+  string &meal = food;
+
+  cout << food << "\n";
+  cout << meal << "\n";
+  return 0;
+}
